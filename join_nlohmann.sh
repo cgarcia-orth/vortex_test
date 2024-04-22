70870b193514c71668fbf573d857f001e6b74fd5
Carlos Garcia <cgarcia@L-CGARCIA>
2024-04-22 14:52:26 -0400

    added split join big file script

---
+++ join_nlohmann.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+cat sam_packages/git2_github.com.nlohmann.json.git.tar.gza* > sam_packages/git2_github.com.nlohmann.json.git.tar.gz
---
+++ split_nlohmann.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+split -b 90M sam_packages/git2_github.com.nlohmann.json.git.tar.gz "sam_packages/git2_github.com.nlohmann.json.git.tar.gz"
